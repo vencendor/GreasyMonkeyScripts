f21de57c4a991297e85ecc8cce778393c1fbba62
Maxim Uglov <wzcc@mail.ru>
2020-02-15 23:58:14 +0100

    Update kinopoiskt.js

--- kinopoiskt.js
+++ kinopoiskt.js
@@ -1,19 +1,18 @@
 // ==UserScript==
-// @name     Script Img hidder
+// @name     Kinopoisk
 // @version  1
 // @grant    none
-// @include  https://www.kinopoisk.ru/film/
+// @include  https://www.kinopoisk.ru/film/*
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
 // ==/UserScript==
 
 
-$(document).ready(function() {  
-    
+$(document).ready(function() { 
+     
   var film_name = $('h1.moviename-big span').html(); 
   console.log( film_name );
-  $('h1.moviename-big span').appendAffter("<a href='https://rezka.ag/index.php?do=search&subaction=search&q="
+  $('h1.moviename-big span').append("<a target='_blank' style='font-size:12px;padding-left:30px;  color: #0000ff; ' href='https://rezka.ag/index.php?do=search&subaction=search&q="
   + film_name +"'>" + film_name + "</a>");
  
   
 });
-
